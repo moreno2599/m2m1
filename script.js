e4a84aecba40414967f306e64c0c0d252ac4375c
moreno2599 <146750546+moreno2599@users.noreply.github.com>
2023-10-12 15:07:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let ilMioNome
+let eta = 24
+
+let citta = " regno delle fate "
+let Professione = " operaio in fabbrica"
+ilMioNome = "moreno "
+
+console.log(ilMioNome)
+console.log(eta)
+console.log(Professione)
+console.log(citta)
+
+//(DOMANDA1)un algoritmo è un insieme di passaggi per arrivare ad un risultato, risolvere uno specifico problema
+//consiste nell' insieme di istruzioni che permettono,passo dopo passo , di svolgere un compito.
+//in cucina, un esempio di algoritmo può essere la preparzione di una torta in cui gli ingredienti devono 
+//essere uniti in un determinato ordine  per la realizzazione della torta.
+
+//(DOMANDA2)una variabile è come un contenitore vuoto a cui bisogna aggiungere un contenuto  
+
+//(DOMANDA3)undifined è un messaggio da parte del nostro pc che alla variabile non è stata assegnato un valore
+//null invece è un valore che noi assegniamo alla variabile per indicare un valore vuoto.
